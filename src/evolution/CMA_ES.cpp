95c5b67e19a6ea684eafbb359b7ab02d332595bd
Ipsedo <berriens@gmail.com>
2019-07-20 01:45:17 +0200

    continué CMA-ES -> stuck dans comprehension de l'article ...

--- src/evolution/CMA_ES.cpp
+++ src/evolution/CMA_ES.cpp
@@ -3,3 +3,82 @@
 //
 
 #include "CMA_ES.h"
+
+#include <algorithm>
+#include <chrono>
+#include "../algebra/operators.h"
+#include "../algebra/functions.h"
+
+using algebra::operator*;
+using algebra::operator+;
+
+CMA_ES::CMA_ES(coco_problem_s *p) :
+    __generator((unsigned long)(std::chrono::duration_cast<std::chrono::milliseconds>
+            (std::chrono::system_clock::now().time_since_epoch()).count())),
+    __normal_dist(0., 1.), __uniform_dist(0., 1.),
+    __problem(p),
+    __min_values(coco_problem_get_smallest_values_of_interest(p)),
+    __max_values(coco_problem_get_largest_values_of_interest(p)),
+    __n(static_cast<int>(coco_problem_get_dimension(p))), __lambda(10), __mu(__lambda / 2),
+    __x(__init_x()),
+    __w(__init_w(vector<individual>())), //TODO piger algo : log rank - w_k
+    __mu_w(1.0 / algebra::sum(__w  * __w)),
+    __c_sigma(__mu_w / (double(__n) + __mu_w)),
+    __d(1.0 + sqrt(__mu_w / double(__n))),
+    __c_c((4.0 + __mu_w / double(__n)) / (double(__n) + 4 + 2 * __mu_w / double(__n))),
+    __c_1(2.0 / (double(__n * __n) + __mu_w)),
+    __c_mu(__mu_w / (double(__n * __n) + __mu_w)),
+    __c_m(1.0),
+    __s_sigma(vector<double>(static_cast<unsigned long>(__n), 0.0)),
+    __s_c(vector<double>(static_cast<unsigned long>(__n), 0.0)),
+    __C(algebra::identity(__n)),
+    __sigma(algebra::rand(__uniform_dist, __generator, __n, 1e-5, 2.0)) {
+
+}
+
+individual CMA_ES::__init_x() {
+    vector<double> res(static_cast<unsigned long>(__n));
+
+    for (int i = 0; i < __n; i++)
+        res[i] = __uniform_dist(__generator) * (__max_values[i] - __min_values[i]) + __min_values[i];
+
+    double fitness;
+    coco_evaluate_function(__problem, res.data(), &fitness);
+
+    return {res, fitness};
+}
+
+vector<double> CMA_ES::__init_w(std::vector<individual> pop) {
+    vector<double> w_prime(static_cast<unsigned long>(__mu));
+
+    for (int k = 0; k < __mu; k++)
+        w_prime[k] = log(static_cast<double>(__lambda) / 2.0 + 0.5) - log(pop[k].fitness); // log rank(f(x_k)) ???
+
+    vector<double> w(static_cast<unsigned long>(__mu));
+
+    double sum = algebra::sum(w_prime);
+
+    for (int k = 0; k < __mu; k++)
+        w[k] = w_prime[k] / sum;
+
+    return w;
+}
+
+void CMA_ES::step() {
+    vector<algebra::matrix> z(static_cast<unsigned long>(__lambda));
+    vector<individual> childs(static_cast<unsigned long>(__lambda));
+
+    for (int k = 0; k < __lambda; k++) {
+        z[k] = algebra::normal_identity(__normal_dist, __n);
+        childs[k].geno = __x.geno + 0.0; //TODO compute square root matrix :,(
+
+        coco_evaluate_function(__problem, childs[k].geno.data(), &childs[k].fitness);
+    }
+
+    sort(childs.begin(), childs.end(), [](individual i1, individual i2){ return i1.fitness < i2.fitness; });
+    vector<individual> pop(&childs[0], &childs[__mu]);
+
+    __w = __init_w(pop);
+    __x.geno = algebra::dot(__sigma, algebra::sum(z * __w) *__C * __c_m * __sigma + __x.geno); //TODO compute square root matrix :,(
+
+}
--- src/evolution/CMA_ES.h
+++ src/evolution/CMA_ES.h
@@ -5,9 +5,64 @@
 #ifndef EVOMOTION_CMA_ES_H
 #define EVOMOTION_CMA_ES_H
 
+#include <vector>
+#include <random>
+#include "../coco.h"
+#include "individual.h"
+#include "../algebra/matrix.h"
+
+using namespace std;
 
 class CMA_ES {
+private:
+    /*
+     * std::random stuff
+     */
+    normal_distribution<double> __normal_dist;
+    uniform_real_distribution<double> __uniform_dist;
+    default_random_engine __generator;
 
+    /*
+     * COCO stuff
+     */
+    coco_problem_t *__problem;
+    const double *__min_values;
+    const double *__max_values;
+
+    /*
+     * CMA-ES stuff
+     */
+
+    int __n;
+
+    int __lambda;
+    int __mu;
+
+    individual __x;
+
+    vector<double> __w;
+
+    double __mu_w;
+    double __c_sigma;
+    double __d;
+    double __c_c;
+    double __c_1;
+    double __c_mu;
+    double __c_m;
+
+    vector<double> __s_sigma;
+    vector<double> __s_c;
+    algebra::matrix __C;
+    vector<double> __sigma;
+
+    individual __init_x();
+
+    vector<double> __init_w(std::vector<individual> pop);
+
+public:
+    explicit CMA_ES(coco_problem_s *p);
+
+    void step();
 };
 
 
