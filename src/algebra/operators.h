2f1a76ae30447af49e5efed70ed23eabde91dfe3
Ipsedo <berriens@gmail.com>
2019-07-20 01:42:38 +0200

    renamed

---
+++ src/algebra/operators.h
@@ -0,0 +1,61 @@
+//
+// Created by samuel on 19/07/19.
+//
+
+#ifndef EVOMOTION_BASIC_OPERATORS_H
+#define EVOMOTION_BASIC_OPERATORS_H
+
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+
+#define SIZE_ERROR 2
+
+namespace algebra {
+
+    using namespace std;
+
+    template<typename T>
+    vector<T> operator*(vector<T> v1, vector<T> v2) {
+        if (v1.size() != v2.size()) {
+            cout << "Size " << v1.size()
+                 << " and " << v2.size()
+                 << " are incompatible" << endl;
+            exit(SIZE_ERROR);
+        }
+        vector<T> res;
+        for (int i = 0; i < v1.size(); i++)
+            res.push_back(v1[i] * v2[i]);
+        return res;
+    }
+
+    template<typename T, typename U>
+    vector<T> operator*(vector<T> v, U s) {
+        transform(v.begin(), v.end(), v.begin(), [&s](T t) { return t * s; });
+        return v;
+    }
+
+    template<typename T>
+    vector<T> operator+(vector<T> v1, vector<T> v2) {
+        if (v1.size() != v2.size()) {
+            cout << "Size " << v1.size()
+                 << " and " << v2.size()
+                 << " are incompatible" << endl;
+            exit(SIZE_ERROR);
+        }
+        vector<T> res;
+        for (int i = 0; i < v1.size(); i++)
+            res.push_back(v1[i] + v2[i]);
+        return res;
+    }
+
+    template<typename T, typename U>
+    vector<T> operator+(vector<T> v, U s) {
+        transform(v.begin(), v.end(), v.begin(), [&s](T t) { return t + s; });
+        return v;
+    }
+
+}
+
+#endif //EVOMOTION_BASIC_OPERATORS_H
