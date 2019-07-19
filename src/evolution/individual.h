1f89f95fc002a64d115549f8a48867e05dfcc57b
Ipsedo <berriens@gmail.com>
2019-07-20 01:44:40 +0200

    add individual definition

---
+++ src/evolution/individual.h
@@ -0,0 +1,15 @@
+//
+// Created by samuel on 19/07/19.
+//
+
+#ifndef EVOMOTION_INDIVIDUAL_H
+#define EVOMOTION_INDIVIDUAL_H
+
+#include <vector>
+
+struct individual {
+    std::vector<double> geno;
+    double fitness;
+};
+
+#endif //EVOMOTION_INDIVIDUAL_H
