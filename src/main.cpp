a20ad26f4bd7dc1e9af6668759cad0a061094b2d
Ipsedo <berriens@gmail.com>
2019-07-20 01:45:48 +0200

    test dot(vector, matrix) -> OK

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
-#include "tests/vector.h"
+#include "tests/vector_test.h"
+#include "tests/coco_test.h"
+#include "tests/matrix_test.h"
 
 int main() {
     tests_vector_operator();
+    test_matrix();
     return 0;
 }
