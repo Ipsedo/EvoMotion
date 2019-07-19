ad5bd7ad11884f0c6bcb81c9c3119cbb4c7d211c
Ipsedo <berriens@gmail.com>
2019-07-19 19:46:04 +0200

    plus qu'à implementer CMA-ES

---
+++ src/evolution/CMA_ES.cpp
@@ -0,0 +1,5 @@
+//
+// Created by samuel on 19/07/19.
+//
+
+#include "CMA_ES.h"
---
+++ src/evolution/CMA_ES.h
@@ -0,0 +1,14 @@
+//
+// Created by samuel on 19/07/19.
+//
+
+#ifndef EVOMOTION_CMA_ES_H
+#define EVOMOTION_CMA_ES_H
+
+
+class CMA_ES {
+
+};
+
+
+#endif //EVOMOTION_CMA_ES_H
