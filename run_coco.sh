c2912c99758e7e19ef04bce9bdbb5888964630a9
Ipsedo <berriens@gmail.com>
2019-07-22 20:27:40 +0200

    add run coco script

---
+++ run_coco.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+echo "Build"
+bash ./build.sh
+
+rm -rf exdata/
+echo "Removed exdata/"
+
+rm -rf ppdata/
+echo "Removed ppdata/"
+
+echo "Start Algo"
+./build/EvoMotion
+python -m cocopp exdata/CMA_on_bbob
+
+cd ppdata
+firefox index.html
+cd ..
