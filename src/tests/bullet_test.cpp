3ed06d55132daff4e89453c51caffa6997ebbd88
Ipsedo <berriens@gmail.com>
2019-08-11 00:25:54 +0200

    bullet include OK

---
+++ src/tests/bullet_test.cpp
@@ -0,0 +1,12 @@
+//
+// Created by samuel on 11/08/19.
+//
+
+#include "bullet_test.h"
+
+#include <btBulletDynamicsCommon.h>
+#include <iostream>
+
+void test_bullet() {
+    std::cout << "Bullet test" << std::endl;
+}
---
+++ src/tests/bullet_test.h
@@ -0,0 +1,10 @@
+//
+// Created by samuel on 11/08/19.
+//
+
+#ifndef EVOMOTION_BULLET_TEST_H
+#define EVOMOTION_BULLET_TEST_H
+
+void test_bullet();
+
+#endif //EVOMOTION_BULLET_TEST_H
