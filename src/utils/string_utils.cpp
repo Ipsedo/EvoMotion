c432acf036863f243c731b3768c1cbb6637be3a5
Ipsedo <berriens@gmail.com>
2019-08-11 02:56:52 +0200

    add string stuff

---
+++ src/utils/string_utils.cpp
@@ -0,0 +1,18 @@
+//
+// Created by samuel on 11/08/19.
+//
+
+#include "string_utils.h"
+
+#include <sstream>
+
+std::vector<std::string> split(const std::string &s, char delim) {
+    std::stringstream ss(s);
+    std::string item;
+    std::vector<std::string> elems;
+    while (std::getline(ss, item, delim)) {
+        //elems.push_back(item);
+        elems.push_back(move(item)); // if C++11 (based on comment from @mchiasson)
+    }
+    return elems;
+}
---
+++ src/utils/string_utils.h
@@ -0,0 +1,13 @@
+//
+// Created by samuel on 11/08/19.
+//
+
+#ifndef EVOMOTION_STRING_UTILS_H
+#define EVOMOTION_STRING_UTILS_H
+
+#include <string>
+#include <vector>
+
+std::vector<std::string> split(const std::string &s, char delim);
+
+#endif //EVOMOTION_STRING_UTILS_H
