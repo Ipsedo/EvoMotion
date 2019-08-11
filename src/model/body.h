9531cd249b0ef2d9215e6e5160c50dffebe1653a
Ipsedo <berriens@gmail.com>
2019-08-11 19:14:26 +0200

    add item def + create box fun

---
+++ src/model/body.h
@@ -0,0 +1,22 @@
+//
+// Created by samuel on 11/08/19.
+//
+
+#ifndef EVOMOTION_BODY_H
+#define EVOMOTION_BODY_H
+
+#include <btBulletDynamicsCommon.h>
+#include "../view/obj_mtl_vbo.h"
+
+struct item {
+    btRigidBody *m_rg_body;
+    ObjMtlVBO m_obj_mtl_vbo;
+    glm::vec3 m_obj_mtl_vbo_scale;
+};
+
+btRigidBody::btRigidBodyConstructionInfo
+localCreateInfo(btCollisionShape *collision_shape, glm::vec3 pos, glm::mat4 rot_mat, glm::vec3 scale, float mass);
+
+item create_item_box(glm::vec3 pos, glm::mat4 rot_mat, glm::vec3 scale, float mass);
+
+#endif //EVOMOTION_BODY_H
