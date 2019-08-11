2dda2bc8ba12d19e29492ed4f6252269a636eb40
Ipsedo <berriens@gmail.com>
2019-08-11 19:14:57 +0200

    add gl error function

---
+++ src/view/error.h
@@ -0,0 +1,10 @@
+//
+// Created by samuel on 11/08/19.
+//
+
+#ifndef EVOMOTION_ERROR_H
+#define EVOMOTION_ERROR_H
+
+void error_callback(int error, const char *description);
+
+#endif //EVOMOTION_ERROR_H
