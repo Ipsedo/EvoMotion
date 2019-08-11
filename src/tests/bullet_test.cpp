dffc5e02f069c988fd5cf2b7c60a90fd66059e30
Ipsedo <berriens@gmail.com>
2019-08-11 19:16:14 +0200

    bullet test ok

--- src/tests/bullet_test.cpp
+++ src/tests/bullet_test.cpp
@@ -4,9 +4,14 @@
 
 #include "bullet_test.h"
 
-#include <btBulletDynamicsCommon.h>
+#include "../core/env_list.h"
 #include <iostream>
 
 void test_bullet() {
     std::cout << "Bullet test" << std::endl;
+
+    environment env_box = create_test_env();
+
+    while (env_box.will_draw)
+        env_box.step(1.f / 60.f);
 }
