6f67b5bca91decb8a28c9c11670b20963acfeb87
Ipsedo <berriens@gmail.com>
2019-08-12 11:44:27 +0200

    create rl test file

---
+++ src/tests/rl_test.cpp
@@ -0,0 +1,17 @@
+//
+// Created by samuel on 12/08/19.
+//
+
+#include "rl_test.h"
+#include "../core/env_list.h"
+
+void test_reinforcement_learning() {
+	std::cout << "Bullet test" << std::endl;
+
+	environment env_box = cartpole_env();
+
+	while (env_box.will_draw) {
+		env_step new_state = env_box.step(1.f / 60.f, torch::rand(1) * 2.f - 1.f);
+		std::cout << new_state.state << std::endl;
+	}
+}
---
+++ src/tests/rl_test.h
@@ -0,0 +1,10 @@
+//
+// Created by samuel on 12/08/19.
+//
+
+#ifndef EVOMOTION_RL_TEST_H
+#define EVOMOTION_RL_TEST_H
+
+void test_reinforcement_learning();
+
+#endif //EVOMOTION_RL_TEST_H
