686100f90804ac026811de56b3d6d8bb202437d0
Ipsedo <berriens@gmail.com>
2019-08-13 03:01:48 +0200

    use is_on

--- src/tests/bullet_test.cpp
+++ src/tests/bullet_test.cpp
@@ -12,8 +12,8 @@ void test_bullet() {
 
     environment env_box = create_test_env();
 
-    while (env_box.will_draw) {
-        env_step new_state = env_box.step(1.f / 60.f, torch::rand(1) * 2.f - 1.f);
+    while (env_box.m_renderer.m_is_on) {
+        env_step new_state = env_box.step(1.f / 60.f, torch::rand(1) * 2.f - 1.f, true);
         std::cout << new_state.state << std::endl;
     }
 }
