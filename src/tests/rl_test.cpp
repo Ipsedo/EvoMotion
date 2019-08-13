c4d4c02d0f3366c2d3460efe073505862186dc5a
Ipsedo <berriens@gmail.com>
2019-08-13 03:57:56 +0200

    env rl ok, restart view ok -> go RL

--- src/tests/rl_test.cpp
+++ src/tests/rl_test.cpp
@@ -10,15 +10,21 @@ void test_reinforcement_learning() {
 
 	environment cartpole_env = create_cartpole_env();
 
-	int nb_try = 10;
+	std::cout << "Action space : " << cartpole_env.m_actions_sizes << std::endl;
+	std::cout << "State space : " << cartpole_env.m_state_sizes << std::endl;
+
+	int nb_try = 20;
 
 	bool done = false;
 
 	for (int i = 0; i < nb_try; i++) {
 		while (!done) {
-			env_step new_state = cartpole_env.step(1.f / 60.f, torch::rand(1) * 2.f - 1.f, i > 5);
-			std::cout << new_state.state << std::endl;
+			//bool will_draw = i < 5 ? true : (i > 15 ? true : cartpole_env.m_renderer.m_is_on); // test
+			bool will_draw = true;
+			env_step new_state = cartpole_env.step(1.f / 60.f, torch::rand(1) * 2.f - 1.f, will_draw);
+			//std::cout << new_state.state << std::endl;
 			done = new_state.done;
+			std::cout << "Episode : " << i << ", reward = " << new_state.reward << std::endl;
 		}
 		cartpole_env.reset();
 		done = false;
