f2b07f3f4aebd64d0b6e0c677fa9d68dafd4e1f8
Ipsedo <berriens@gmail.com>
2019-08-13 18:18:43 +0200

    test rl & DQN ok -> même trop fort il perd jamais

--- src/tests/rl_test.cpp
+++ src/tests/rl_test.cpp
@@ -4,30 +4,57 @@
 
 #include "rl_test.h"
 #include "../core/env_list.h"
+#include "../algos/rl/agent.h"
 
 void test_reinforcement_learning() {
 	std::cout << "Reinforcement learning test" << std::endl;
 
 	environment cartpole_env = create_cartpole_env();
 
+    auto ag = dqn_agent(cartpole_env.m_state_sizes, cartpole_env.m_actions_sizes);
+
 	std::cout << "Action space : " << cartpole_env.m_actions_sizes << std::endl;
 	std::cout << "State space : " << cartpole_env.m_state_sizes << std::endl;
 
-	int nb_try = 20;
-
-	bool done = false;
+	int nb_try = 200;
 
 	for (int i = 0; i < nb_try; i++) {
-		while (!done) {
-			//bool will_draw = i < 5 ? true : (i > 15 ? true : cartpole_env.m_renderer.m_is_on); // test
-			bool will_draw = true;
-			env_step new_state = cartpole_env.step(1.f / 60.f, torch::rand(1) * 2.f - 1.f, will_draw);
-			//std::cout << new_state.state << std::endl;
-			done = new_state.done;
-			std::cout << "Episode : " << i << ", reward = " << new_state.reward << std::endl;
+		env_step state = cartpole_env.get_first_state();
+
+		float cumulative_reward = 0.f;
+
+		int step = 0;
+
+		//float eps = float(1. / (log(i + 1.) / log(2.f)));
+		float eps = 1e-3f;
+
+		while (!state.done && step < 300) {
+			auto act = ag.act(state.state, eps);
+
+			env_step new_state = cartpole_env.step(1.f / 60.f, act, true);
+
+			ag.step(state.state, act, new_state.reward, new_state.state, new_state.done);
+
+			state = new_state;
+
+			cumulative_reward += state.reward;
+
+			step++;
 		}
 		cartpole_env.reset();
-		done = false;
-		std::cout << "reset" << std::endl;
+		std::cout << "Episode (" << i << ") : cumulative_reward = " << cumulative_reward << std::endl;
+	}
+
+	env_step state = cartpole_env.get_first_state();
+
+	while (!state.done) {
+		auto act = ag.act(state.state, 1e-3f);
+
+		env_step new_state = cartpole_env.step(1.f / 60.f, act, true);
+
+		//ag.step(state.state, act, new_state.reward, new_state.state, new_state.done);
+
+		state = new_state;
+
 	}
 }
