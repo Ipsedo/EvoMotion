d6249b20ddc271d4618cf96a24a6e621a38f3280
Ipsedo <berriens@gmail.com>
2019-08-14 23:11:03 +0200

    use class for env + dqn trop instable...

--- src/tests/rl_test.cpp
+++ src/tests/rl_test.cpp
@@ -5,40 +5,42 @@
 #include "rl_test.h"
 #include "../core/env_list.h"
 #include "../algos/rl/deep_q_learning.h"
+#include <chrono>
 
 void test_reinforcement_learning() {
 	std::cout << "Reinforcement learning test" << std::endl;
 
-	environment cartpole_env = create_cartpole_env();
+	Environment *cartpole_env = new CartPoleEnv(static_cast<int>(time(nullptr)));
 
-    auto ag = dqn_agent(cartpole_env.m_state_sizes, cartpole_env.m_actions_sizes);
+    agent *ag = new dqn_agent(static_cast<int>(time(nullptr)),
+    		cartpole_env->state_space(), cartpole_env->action_space());
 
-	std::cout << "Action space : " << cartpole_env.m_actions_sizes << std::endl;
-	std::cout << "State space : " << cartpole_env.m_state_sizes << std::endl;
+	std::cout << "Action space : " << cartpole_env->action_space() << std::endl;
+	std::cout << "State space : " << cartpole_env->state_space() << std::endl;
 
 	int nb_episode = 300;
 	int max_episode_step = 300;
 	int consecutive_succes = 0;
 
 	float eps = 0.5f;
-	float eps_decay = 0.9995f;
-	float eps_min = 5e-2f;
+	float eps_decay = 0.99995f;
+	float eps_min = 1e-1f;
 
 	int step = 0;
 
 	for (int i = 0; i < nb_episode; i++) {
-		env_step state = cartpole_env.get_first_state();
+		env_step state = cartpole_env->reset();
 
 		float cumulative_reward = 0.f;
 
 		int episode_step = 0;
 
 		while (!state.done && episode_step < max_episode_step) {
-			auto act = ag.act(state.state, eps);
+			auto act = ag->act(state.state, eps);
 
-			env_step new_state = cartpole_env.step(1.f / 60.f, act, false);
+			env_step new_state = cartpole_env->step(1.f / 60.f, act, false);
 
-			ag.step(state.state, act, new_state.reward, new_state.state, new_state.done);
+			ag->step(state.state, act, new_state.reward, new_state.state, new_state.done);
 
 			state = new_state;
 
@@ -54,31 +56,30 @@ void test_reinforcement_learning() {
 		if (episode_step >= max_episode_step) consecutive_succes++;
 		else consecutive_succes = 0;
 
-		if (consecutive_succes > 10) { cartpole_env.reset(); break; }
+		if (consecutive_succes > 10) break;
 
-		cartpole_env.reset();
 		std::cout << std::fixed << std::setprecision(5)
 		<< "Episode (" << std::setw(3) << i << ") : cumulative_reward = " << std::setw(9) << cumulative_reward
-		<< ", eps = " << std::setw(9) << eps << std::endl;
+		<< ", eps = " << std::setw(6) << eps << ", episode step : " << std::setw(3) << episode_step << std::endl;
 	}
 
-	int nb_test = 0;
+	int nb_test = 100;
 
 	for (int i = 0; i < nb_test; i++) {
 
-		env_step state = cartpole_env.get_first_state();
+		env_step state = cartpole_env->reset();
 
 		while (!state.done) {
-			auto act = ag.act(state.state, 0.f);
+			auto act = ag->act(state.state, 0.f);
 
-			env_step new_state = cartpole_env.step(1.f / 60.f, act, true);
+			env_step new_state = cartpole_env->step(1.f / 60.f, act, true);
 
 			//ag.step(state.state, act, new_state.reward, new_state.state, new_state.done);
 
 			state = new_state;
 
 		}
-
-		cartpole_env.reset();
 	}
+
+	delete cartpole_env;
 }
