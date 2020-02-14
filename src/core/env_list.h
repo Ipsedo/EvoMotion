21434f0b938afbab602c151480874132cb65cca6
Ipsedo <berriens@gmail.com>
2020-02-14 18:09:26 +0100

    EnvEnum created

--- src/core/env_list.h
+++ src/core/env_list.h
@@ -7,8 +7,28 @@
 
 // group all environment
 
-#include "envs/cartpole.h"
-#include "envs/test.h"
-#include "envs/pendulum.h"
+#include "environment.h"
+
+class EnvEnum {
+public:
+    static EnvEnum CONTINUOUS_CARTPOLE();
+    static EnvEnum PENDULUM();
+    static EnvEnum DISCRETE_CARTPOLE();
+    static EnvEnum HUNTING();
+
+
+private:
+    const std::string name;
+    const std::function<Environment *()> get_env_fun;
+    EnvEnum(std::string name, std::function<Environment *()> get_env_fun);
+
+public:
+
+    std::string get_name() const ;
+    Environment * get_env() const ;
+
+    static EnvEnum from_str(std::string env_value);
+
+};
 
 #endif //EVOMOTION_ENV_LIST_H
