40cc4bb97ce6f020d46f5838f5c104ebb83700a6
samuel <berriens@gmail.com>
2022-12-08 19:43:21 +0100

    new python branch

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-if [ -d "./build" ]; then
-	rm -rf ./build
-fi
-mkdir build
-cd build
-cmake ..
-make -j 8
-cd ..
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="evo_motion",
+    version="1.0.0",
+    author="Samuel Berrien",
+    packages=["evo_motion"],
+    license="GPL-3.0 License",
+)
