2fa0bf369071ac9c7be42a759f421beddbc8e273
Ipsedo <berriens@gmail.com>
2024-05-06 00:29:19 +0200

    reformat

--- evo_motion_networks/src/agents/debug_agents.cpp
+++ evo_motion_networks/src/agents/debug_agents.cpp
@@ -19,7 +19,8 @@ void DebugAgent::set_eval(bool eval) {}
 
 // Random Agent
 RandomAgent::RandomAgent(
-    const int seed, const std::vector<int64_t> &state_space, const std::vector<int64_t> &action_space,
+    const int seed, const std::vector<int64_t> &state_space,
+    const std::vector<int64_t> &action_space,
     const int hidden_size, const float lr)
     : DebugAgent(seed, state_space, action_space, hidden_size, lr) {}
 
@@ -29,7 +30,8 @@ torch::Tensor RandomAgent::act(torch::Tensor state, float reward) {
 
 // Constant Agent
 ConstantAgent::ConstantAgent(
-    const int seed, const std::vector<int64_t> &state_space, const std::vector<int64_t> &action_space,
+    const int seed, const std::vector<int64_t> &state_space,
+    const std::vector<int64_t> &action_space,
     const int hidden_size, const float lr)
     : DebugAgent(seed, state_space, action_space, hidden_size, lr) {}
 torch::Tensor ConstantAgent::act(torch::Tensor state, float reward) {
