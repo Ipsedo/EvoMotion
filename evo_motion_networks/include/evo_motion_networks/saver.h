c27c39146f26d69c2110bd7dff7a2dcbea575513
Ipsedo <berriens@gmail.com>
2024-12-06 12:05:13 +0100

    add check for reading file

--- evo_motion_networks/include/evo_motion_networks/saver.h
+++ evo_motion_networks/include/evo_motion_networks/saver.h
@@ -13,6 +13,10 @@
 template<typename T>
 void save_torch(const std::string &output_folder_path, T to_save, const std::string &file_name) {
     const std::filesystem::path path(output_folder_path);
+
+    if (!std::filesystem::exists(path))
+        throw std::runtime_error("Could not find " + output_folder_path);
+
     const auto file = path / file_name;
     torch::serialize::OutputArchive archive;
     to_save->save(archive);
@@ -23,6 +27,10 @@ template<typename T>
 void load_torch(const std::string &input_folder_path, T to_fill, const std::string &file_name) {
     const std::filesystem::path path(input_folder_path);
     const auto file = path / file_name;
+
+    if (!std::filesystem::exists(file))
+        throw std::runtime_error("Could not find " + input_folder_path);
+
     torch::serialize::InputArchive archive;
     archive.load_from(file);
     to_fill->load(archive);
