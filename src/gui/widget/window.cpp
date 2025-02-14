c42ea6770362211884354b5e15be5d5fbef8f382
Ipsedo <berriens@gmail.com>
2025-02-14 10:45:09 +0100

    rename file

--- src/gui/widget/window.cpp
+++ src/gui/widget/window.cpp
@@ -2,11 +2,11 @@
 // Created by samuel on 11/02/25.
 //
 
+#include "./window.h"
+
 #include <utility>
 
 #include <imgui.h>
-
-#include "./window.h"
 
 ImGuiWindow::ImGuiWindow(std::string name) : name(std::move(name)), show(true), focus(false) {}
 
